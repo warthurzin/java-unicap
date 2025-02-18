225a7a451dda0cae2207cca2df99146e05907eec
Arthur  Silveira <arthurmarques2148@gmail.com>
2025-02-18 23:46:14 +0000

    Programa 01 - exemplo de funções

---
+++ ex01-hello-world/Exemplo01.java
@@ -0,0 +1,14 @@
+public class Exemplo01{
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        System.out.println("O dobro de 10.5 é " + dobro(10.5));
+    }
+
+    public static int dobro(int num) {
+        return 2 * num;
+    }
+
+    public static double dobro(double num) {
+        return 2 * num;
+    }
+}
